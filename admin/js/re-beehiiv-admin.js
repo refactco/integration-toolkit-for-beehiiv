6634a3666b1284bb2157c776a4348b66d3d7bc3b
Mohammad Zarei <mohammad.zarei1380@gmail.com>
2023-05-10 15:55:05 +0330

    1204537332878286 [Feat]: Add styles and scripts

--- admin/js/re-beehiiv-admin.js
+++ admin/js/re-beehiiv-admin.js
@@ -28,8 +28,41 @@
    * Although scripts in the WordPress core, Plugins and Themes may be
    * practising this, we should strive to set a better example in our own work.
    */
+  function wp_faculty_refresh_request() {
+    console.log('wp_faculty_refresh_request');
+    console.log(SA_CORE);
 
+
+    var data = {
+      action: 're_beehiiv_import',
+      nonce: $('#RE_BEEHIIV_ajax_import-nonce').val(),
+      content_type: $('#wp-faculty-content_type').val(),
+      cat: $('#wp-faculty-category').val(),
+    };
+
+    jQuery.post(SA_CORE.ajax_url, data, function (response) {
+      var progressbar = $('#wp-faculty-progress').find('.cssProgress-bar');
+      var percent = parseInt(response.percent);
+      var last_id = parseInt(response.last_id);
+      var count = parseInt(response.count);
+      var text = '(' + last_id + ' / ' + count + ') ' + percent + '%';
+      progressbar.css({ 'width': percent + '%' });
+      progressbar.find('.cssProgress-label').text(text);
+      if (percent < 100) {
+          wp_faculty_refresh_request();
+      }
+      if (percent == 100) {
+          progressbar.removeClass('cssProgress-active');
+      }
+    });
+  }
   jQuery(document).ready(function ($) {
-    // Your code here
+    $('#wp-faculty-start-import').on('click', function () {
+        var progressbar = $('#wp-faculty-progress').find('.cssProgress-bar');
+        progressbar.addClass('cssProgress-active');
+        $(this).hide();
+        $('#wp-faculty-pause-import').show();
+        wp_faculty_refresh_request();
+    });
   });
 })(jQuery);
