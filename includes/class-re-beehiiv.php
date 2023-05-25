674d77ee2f7ba5e933c5636a0715e1c7f3f7a267
saeed jamali <saeed.ja@markitasdone.com>
2023-05-25 18:25:37 +0330

    #1204646252925903 remove gravity forms integration

--- includes/class-re-beehiiv.php
+++ includes/class-re-beehiiv.php
@@ -188,7 +188,6 @@ class Re_Beehiiv
 		 * Block Hooks
 		 */
 		$this->loader->add_action('init', \Re_Beehiiv\Blocks\Blocks::class, 'register_all_blocks', 10);
-		$this->loader->add_action('init', \Re_Beehiiv\GravityForms\GravityForms::class, 'init', 11);
 	}
 
 	/**
