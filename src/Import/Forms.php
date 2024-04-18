cd0aa86e2ab408623d2f43c2a181accb73b1bdee
Saeed Jamali <saeed.ja@refact.co>
2024-04-18 12:32:46 +0330

    #1206947300793965 Fix : Nonces and User Permissions Needed for Security

--- src/Import/Forms.php
+++ src/Import/Forms.php
@@ -59,45 +59,44 @@ class Forms {
 	);
 
 
+	/**
+	 * Register the auto import form.
+	 * 
+	 * @return void
+	 */
 	public function maybe_register_auto_import() {
-		if ( isset( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ) ) {
-			$nonce = sanitize_text_field( wp_unslash( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ));
-			if ( ! wp_verify_nonce( $nonce, 'integration_toolkit_for_beehiiv_action' ) ) {
-				return;
-			}
-			$form_data = $this->get_form_validated_data();
+		$form_data = $this->get_form_validated_data();
 
-			if ( isset( $form_data['error'] ) ) {
-				// show the error message
-				add_action(
-					'integration_toolkit_for_beehiiv_admin_notices',
-					function () use ( $form_data ) {
-						?>
-					<div class="notice notice-error">
-						<p><?php echo esc_html__( $form_data['error'] ,'integration-toolkit-for-beehiiv' ); ?></p>
-					</div>
-						<?php
-					}
-				);
-				return;
-			}
-
-			$form_data['api_key']        = get_option( 'integration_toolkit_for_beehiiv_api_key' );
-			$form_data['publication_id'] = get_option( 'integration_toolkit_for_beehiiv_publication_id' );
-			$form_data                   = array(
-				'primary_account' => $form_data,
+		if ( isset( $form_data['error'] ) ) {
+			// show the error message
+			add_action(
+				'integration_toolkit_for_beehiiv_admin_notices',
+				function () use ( $form_data ) {
+					?>
+				<div class="notice notice-error">
+					<p><?php echo esc_html__( $form_data['message'] ,'integration-toolkit-for-beehiiv' ); ?></p>
+				</div>
+					<?php
+				}
 			);
-
-			/**
-			 * Filter the form data before starting the import
-			 *
-			 * @param array $form_data The form data
-			 */
-			$form_data = apply_filters( 'integration_toolkit_for_beehiiv_auto_import_form_data', $form_data );
-			$import    = new Import( $form_data, 'auto_recurring_import', 'auto' );
-			// redirect to import page
-			wp_safe_redirect( admin_url( 'admin.php?page=integration-toolkit-for-beehiiv-import&tab=auto-import' ) );
+			return;
 		}
+
+		$form_data['api_key']        = get_option( 'integration_toolkit_for_beehiiv_api_key' );
+		$form_data['publication_id'] = get_option( 'integration_toolkit_for_beehiiv_publication_id' );
+		$form_data                   = array(
+			'primary_account' => $form_data,
+		);
+
+		/**
+		 * Filter the form data before starting the import
+		 *
+		 * @param array $form_data The form data
+		 */
+		$form_data = apply_filters( 'integration_toolkit_for_beehiiv_auto_import_form_data', $form_data );
+		$import    = new Import( $form_data, 'auto_recurring_import', 'auto' );
+		// redirect to import page
+		wp_safe_redirect( admin_url( 'admin.php?page=integration-toolkit-for-beehiiv-import&tab=auto-import' ) );
 	}
 
 	/**
@@ -109,37 +108,28 @@ class Forms {
 	 * @return void
 	 */
 	public function maybe_start_manual_import() {
+		// get the data from the form
+		$form_data = $this->get_form_validated_data();
 
-		if ( isset( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ) ) {
-			$nonce = sanitize_text_field( wp_unslash( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ) );
-
-			if ( ! wp_verify_nonce( $nonce, 'integration_toolkit_for_beehiiv_action' ) ) {
-				return;
-			}
-
-			// get the data from the form
-			$form_data = $this->get_form_validated_data();
-
-			if ( isset( $form_data['error'] ) ) {
-				// show the error message
-				add_action(
-					'integration_toolkit_for_beehiiv_admin_notices',
-					function () use ( $form_data ) {
-						?>
-					<div class="notice notice-error">
-						<p><?php echo esc_html__( $form_data['error'] ); ?></p>
-					</div>
-						<?php
-					}
-				);
-				return;
-			}
-
-			$import = new Import( $form_data, 'manual_import_' . time(), 'manual' );
-
-			// redirect to import page
-			wp_safe_redirect( admin_url( 'admin.php?page=integration-toolkit-for-beehiiv-import' ) );
+		if ( isset( $form_data['error'] ) ) {
+			// show the error message
+			add_action(
+				'integration_toolkit_for_beehiiv_admin_notices',
+				function () use ( $form_data ) {
+					?>
+				<div class="notice notice-error">
+					<p><?php echo esc_html__( $form_data['message'], 'integration-toolkit-for-beehiiv' ); ?></p>
+				</div>
+					<?php
+				}
+			);
+			return;
 		}
+
+		$import = new Import( $form_data, 'manual_import_' . time(), 'manual' );
+
+		// redirect to import page
+		wp_safe_redirect( admin_url( 'admin.php?page=integration-toolkit-for-beehiiv-import' ) );
 	}
 
 	/**
@@ -149,42 +139,57 @@ class Forms {
 	 * @return array
 	 */
 	private function get_form_validated_data() {
+		if ( isset( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ) ) {
+			$nonce = sanitize_text_field( wp_unslash( $_POST['integration_toolkit_for_beehiiv_import_nonce'] ) );
 
-		$form_data = array();
-
-		foreach ( self::FIELDS as $field ) {
-			$field_name = self::FIELD_PREFIX . $field['name'];
-
-			if ( ! isset( $_POST[ $field_name ] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-				continue;
-			}
-
-			if ( $field['required'] && ( ! isset( $_POST[ $field_name ] ) || empty( $_POST[ $field_name ] ) ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-
+			if ( ! wp_verify_nonce( $nonce, 'integration_toolkit_for_beehiiv_action' ) ) {
 				return array(
 					'error'   => true,
-					'message' => sprintf(
-						// Translators: %s is a placeholder for the field label. This text is displayed when a required field is left blank.
-						__( '%s is required', 'integration-toolkit-for-beehiiv' ),
-						$field['label']
-					),
+					'message' => esc_html__( 'Invalid nonce', 'integration-toolkit-for-beehiiv' ),
 				);
 			}
 
-			$n = explode( '--', $field['name'] );
-			if ( $n[0] === 'post_status' ) {
-				$form_data['post_status'][ $n[1] ] = sanitize_text_field( $_POST[ $field_name ] ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
-				continue;
+			$form_data = array();
+
+			foreach ( self::FIELDS as $field ) {
+				$field_name = self::FIELD_PREFIX . $field['name'];
+
+				if ( ! isset( $_POST[ $field_name ] ) ) {
+					continue;
+				}
+
+				if ( $field['required'] && ( ! isset( $_POST[ $field_name ] ) || empty( $_POST[ $field_name ] ) ) ) {
+
+					return array(
+						'error'   => true,
+						'message' => sprintf(
+							// Translators: %s is a placeholder for the field label. This text is displayed when a required field is left blank.
+							__( '%s is required', 'integration-toolkit-for-beehiiv' ),
+							$field['label']
+						),
+					);
+				}
+
+				$n = explode( '--', $field['name'] );
+				if ( $n[0] === 'post_status' ) {
+					$form_data['post_status'][ $n[1] ] = sanitize_text_field( $_POST[ $field_name ] );
+					continue;
+				}
+
+				if ( is_array( $_POST[ $field_name ] ) ) {
+					$form_data[ $field['name'] ] = array_map( 'sanitize_text_field', $_POST[ $field_name ] );
+				} else {
+					$form_data[ $field['name'] ] = sanitize_text_field( $_POST[ $field_name ] );
+				}
 			}
 
-			if ( is_array( $_POST[ $field_name ] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-				$form_data[ $field['name'] ] = array_map( 'sanitize_text_field', $_POST[ $field_name ] ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
-			} else {
-				$form_data[ $field['name'] ] = sanitize_text_field( $_POST[ $field_name ] ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
-			}
+			return $form_data;
+		} else {
+			return array(
+				'error'   => true,
+				'message' => esc_html__( 'Invalid nonce', 'integration-toolkit-for-beehiiv' ),
+			);
 		}
-
-		return $form_data;
 	}
 
-	}
+}
