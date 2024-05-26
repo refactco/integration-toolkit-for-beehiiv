9b1d53a97f6ad3371a93ffa4836ff76673ab34f3
Masoud Golchin <mgolchin1993@gmail.com>
2024-05-26 14:14:59 +0330

    fix text-domain

--- integration-toolkit-for-beehiiv.php
+++ integration-toolkit-for-beehiiv.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:       Integration Toolkit for beehiiv
  * Plugin URI:        https://refact.co/
@@ -8,7 +9,7 @@
  * Author URI:        https://refact.co
  * License:           GPL2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       Integration Toolkit for beehiiv
+ * Text Domain:       integration-toolkit-for-beehiiv
  * Domain Path:       /languages
  * Requires at least: 6.5.3
  * Requires PHP:      7.4
@@ -17,23 +18,23 @@
  **/
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
 // Load Plugin File autoload
-require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-if ( ! defined( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_CORE_VERSION' ) ) {
-	define( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_CORE_VERSION', '1.0.0' );
+if (!defined('INTEGRATION_TOOLKIT_FOR_BEEHIIV_CORE_VERSION')) {
+	define('INTEGRATION_TOOLKIT_FOR_BEEHIIV_CORE_VERSION', '1.0.0');
 }
 
-if ( ! defined( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_URL' ) ) {
-	define( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_URL', plugin_dir_url( __FILE__ ) );
+if (!defined('INTEGRATION_TOOLKIT_FOR_BEEHIIV_URL')) {
+	define('INTEGRATION_TOOLKIT_FOR_BEEHIIV_URL', plugin_dir_url(__FILE__));
 }
 
-if ( ! defined( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_PATH' ) ) {
-	define( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_PATH', plugin_dir_path( __FILE__ ) );
+if (!defined('INTEGRATION_TOOLKIT_FOR_BEEHIIV_PATH')) {
+	define('INTEGRATION_TOOLKIT_FOR_BEEHIIV_PATH', plugin_dir_path(__FILE__));
 }
 
 
@@ -42,18 +43,19 @@ if ( ! defined( 'INTEGRATION_TOOLKIT_FOR_BEEHIIV_PATH' ) ) {
  * The code that runs during plugin activation.
  * This action is documented in includes/class-integration-toolkit-for-beehiiv-activator.php
  */
-function integration_toolkit_for_beehiiv_activation_hook() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-integration-toolkit-for-beehiiv-activator.php';
+function integration_toolkit_for_beehiiv_activation_hook()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-integration-toolkit-for-beehiiv-activator.php';
 	Integration_Toolkit_For_Beehiiv_Activator::activate();
 }
 
-register_activation_hook( __FILE__, 'integration_toolkit_for_beehiiv_activation_hook' );
+register_activation_hook(__FILE__, 'integration_toolkit_for_beehiiv_activation_hook');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-integration-toolkit-for-beehiiv.php';
+require plugin_dir_path(__FILE__) . 'includes/class-integration-toolkit-for-beehiiv.php';
 
 /**
  * Begins execution of the plugin.
@@ -64,7 +66,8 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-integration-toolkit-for-be
  *
  * @since    1.0.0
  */
-function integration_toolkit_for_beehiiv() {
+function integration_toolkit_for_beehiiv()
+{
 	$plugin = new Integration_Toolkit_For_Beehiiv();
 	$plugin->run();
 }
