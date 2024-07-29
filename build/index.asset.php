633d6163834898d371ebf250ba288a98a88f7699
mahdi-ha-dev <mahdi.ha@markitdone.com>
2024-07-29 10:53:56 +0330

    #1207751706760738 fix: progresbar error

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => '990eee9e023cd8a9fb08');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element'), 'version' => 'a425e47ce45150c3d8ba');
