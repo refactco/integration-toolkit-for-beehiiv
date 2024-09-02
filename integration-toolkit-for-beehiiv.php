8b2fd52414cbc4cbbdddb51c73dea36d566f95db
Saeed Jamali <saeed.ja@refact.co>
2024-09-02 13:47:53 +0330

    chore: Update minimum required version to 6.5.3

--- integration-toolkit-for-beehiiv.php
+++ integration-toolkit-for-beehiiv.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       integration-toolkit-for-beehiiv
  * Domain Path:       /languages
- * Requires at least: 5.5.0
+ * Requires at least: 6.5.3
  * Tested up to:      6.6.1
  * Requires PHP:      7.4
  *
